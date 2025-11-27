c4143241e3b7faac1e6a738fad16118c1565198b
Muhammad Hisham <muhammedheshamm2@gmail.com>
2025-11-27 17:11:44 +0200

    fix/vite config

--- vite.config.ts
+++ vite.config.ts
@@ -20,12 +20,12 @@ export default defineConfig({
         id: "/",
         icons: [
           {
-            src: "/icons/android/android-launchericon-192-192.png",
+            src: "/icons/android-launchericon-192-192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/icons/android/android-launchericon-512-512.png",
+            src: "/icons/android-launchericon-512-512.png",
             sizes: "512x512",
             type: "image/png",
           },
